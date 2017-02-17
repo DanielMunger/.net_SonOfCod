6efaedf707cddf987a34c9139592df28d4019605
Daniel Munger <mungerda@gmail.com>
2017-02-17 12:29:00 -0800

    add home content/styling.

---
+++ src/SonOfCodSeafood/ViewModels/Login.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SonOfCodSeafood.ViewModels
+{
+    public class Login
+    {
+        
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+       
+    }
+}
---
+++ src/SonOfCodSeafood/ViewModels/Register.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace SonOfCodSeafood.ViewModels
+{
+    public class Register
+    {
+        [Required]
+        [Display(Name = "UserName")]
+        public string UserName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Display(Name = "Profile Picture")]
+        public byte[] ProfilePicture { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "The passwords don't match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
